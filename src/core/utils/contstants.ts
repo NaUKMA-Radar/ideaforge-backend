5262dac78ea17465de437a2e7c003fd9aa8b81c2
Oleksandr Igumnov <igumssukr@gmail.com>
2024-09-21 21:14:30 +0300

    Implemented password module and service

---
+++ src/core/utils/contstants.ts
@@ -0,0 +1,18 @@
+export enum ConfigVariables {
+  UserPasswordSaltPrefix = 'USER_PASSWORD_SALT_PREFIX',
+  UserPasswordSaltSuffix = 'USER_PASSWORD_SALT_SUFFIX',
+  UserPasswordSaltRounds = 'USER_PASSWORD_SALT_ROUNDS',
+  JwtSecret = 'JWT_SECRET',
+  JwtAccessTokenDuration = 'JWT_ACCESS_TOKEN_DURATION',
+  DatabaseHost = 'DATABASE_HOST',
+  DatabasePort = 'DATABASE_PORT',
+  DatabaseUsername = 'DATABASE_USERNAME',
+  DatabasePassword = 'DATABASE_PASSWORD',
+  DatabaseName = 'DATABASE_NAME',
+  DatabaseType = 'DATABASE_TYPE',
+}
+
+export enum UserRegistrationMethods {
+  Credentials = 'credentials',
+  Google = 'google',
+}
